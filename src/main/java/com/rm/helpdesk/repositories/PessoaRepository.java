9588714c5e46387d5f5d1002aeb95ef242070e2b
Rodrigo Mascena <mascena19@gmail.com>
2024-09-08 01:12:59 -0300

    Exceção para CPF e E-mail já cadastrados

--- src/main/java/com/rm/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/rm/helpdesk/repositories/PessoaRepository.java
@@ -3,7 +3,11 @@ package com.rm.helpdesk.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.rm.helpdesk.domain.Pessoa;
+import java.util.Optional;
+
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-
+     Optional<Pessoa> findByCpf(String cpf);
+     
+     Optional<Pessoa> findByEmail(String email);
 }
