84f1d1105f1cef61e17f7503c01546b4d6fc1606
Rodrigo Mascena <mascena19@gmail.com>
2024-09-08 00:54:09 -0300

    Criar URL para criação de novo Tecnico

--- src/main/java/com/rm/helpdesk/domain/Tecnico.java
+++ src/main/java/com/rm/helpdesk/domain/Tecnico.java
@@ -2,11 +2,13 @@ package com.rm.helpdesk.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.rm.helpdesk.domain.dtos.TecnicoDTO;
 import com.rm.helpdesk.domain.enums.Perfil;
 
 
@@ -31,6 +33,17 @@ public class Tecnico extends Pessoa {
 		super(id, nome, cpf, email, senha);
 		addPerfil(Perfil.TECNICO);
 		
+	}
+	
+	public Tecnico(TecnicoDTO obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.senha = obj.getSenha();
+		this.perfis = obj.getPerfis().stream().map(x -> x.getCodigo()).collect(Collectors.toSet());
+		this.dataCriacao = obj.getDataCriacao();
 	}
 
 	public List<Chamado> getChamados() {
--- src/main/java/com/rm/helpdesk/services/TecnicoService.java
+++ src/main/java/com/rm/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rm.helpdesk.domain.Tecnico;
+import com.rm.helpdesk.domain.dtos.TecnicoDTO;
 import com.rm.helpdesk.repositories.TecnicoRepository;
 import com.rm.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -23,5 +24,11 @@ public class TecnicoService {
 
 	public List<Tecnico> findAll() {
 		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 }
