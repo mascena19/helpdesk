9d5edbf3e195b6dd6bb633f2d6950ed5b76e6561
Rodrigo Mascena <mascena19@gmail.com>
2024-09-20 00:26:23 -0300

    Criação findByID Chamado

---
+++ src/main/java/com/rm/helpdesk/resources/ChamadoResource.java
@@ -0,0 +1,27 @@
+package com.rm.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rm.helpdesk.domain.Chamado;
+import com.rm.helpdesk.domain.dtos.ChamadoDTO;
+import com.rm.helpdesk.services.ChamadoService;
+
+@RestController
+@RequestMapping(value = "/chamados")
+public class ChamadoResource {
+
+	@Autowired
+	private ChamadoService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
+		Chamado obj = service.findById(id);
+		
+		return ResponseEntity.ok().body(new ChamadoDTO(obj));
+	}
+}
